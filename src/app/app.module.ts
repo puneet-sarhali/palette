bd59c6c4bcfb0045d2c8cc5ca89aa2e578c01637
Puneet Sandhu <pssarhali@gmail.com>
2022-09-07 19:49:48 -0700

    dashboard design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SidebarComponent } from './dashboard/sidebar/sidebar.component';
+import { AvatarComponent } from './dashboard/avatar/avatar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SidebarComponent,
+    AvatarComponent,
   ],
   imports: [
     BrowserModule,
