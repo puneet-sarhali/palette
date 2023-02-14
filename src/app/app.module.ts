79dbbe6876263679aef39dd58ce9798c658ff1af
Puneet Sandhu <pssarhali@gmail.com>
2023-02-13 18:06:06 -0800

    implement hex copy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ClipboardModule } from '@angular/cdk/clipboard';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -55,6 +56,7 @@ import { EmailComponent } from './email/email.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ClipboardModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     //provideAnalytics(() => getAnalytics()),
     provideFirestore(() => getFirestore()),
